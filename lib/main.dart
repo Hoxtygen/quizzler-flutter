bd2938af9a971ff8e9c7214ac1c5c47f906820cc
Wasiu Idowu <hoxtygen@live.com>
2020-09-04 07:09:23 +0100

    Implement Abstraction and Enacapsulation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,10 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  // List<bool> answers = [false, true, true];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +69,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = quizBrain.getQuestionAnswer();
+                if (correctAnswer == true) {
+                  print("You got it right");
+                } else {
+                  print("You got it wrong!");
+                }
+                setState(() {
+                  return quizBrain.nextQuestion();
+                });
               },
             ),
           ),
@@ -80,11 +96,22 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = quizBrain.getQuestionAnswer();
+                if (correctAnswer == false) {
+                  print("You got it right");
+                } else {
+                  print("You got it wrong!");
+                }
+                setState(() {
+                  return quizBrain.nextQuestion();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
